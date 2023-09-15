a7ed5d0ed8a31c9dd01c27310330dcb30c9dd62c
Joey Shi <joeyshi12@gmail.com>
2023-09-15 00:53:50 -0700

    add-multiplayer-page (#1)
    * refactor
    
    * added fake multiplayer

--- supreme_hangman/scenes.py
+++ supreme_hangman/scenes.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import socket
 from typing import List
 import pygame as pg
 import supreme_hangman as shm
@@ -23,10 +24,13 @@ def quit_game():
 class MenuScene(Scene):
     def __init__(self, game):
         self.game = game
+        self.display_messages = []
         self.buttons = [
             ui.Button("Play", 100, 250, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_select_difficulty),
-            ui.Button("Options", 100, 350, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_options),
-            ui.Button("Exit", 100, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, quit_game)
+            #ui.Button("Multiplayer", 100, 320, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_multiplayer),
+            ui.Button("Multiplayer", 100, 320, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__add_single_player_message),
+            ui.Button("Options", 100, 390, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_options),
+            ui.Button("Exit", 100, 460, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, quit_game)
         ]
         pg.mixer.music.load(os.path.join("assets", "sounds", "opening.wav"))
         pg.mixer.music.play(-1)
@@ -40,19 +44,38 @@ class MenuScene(Scene):
         shm.game_display.blit(shm.LOGO_IMAGE, (30, -30))
         shm.game_display.blit(shm.HANGING_KNUCKLES_IMAGE, (450, 250))
         pg.draw.line(shm.game_display, shm.BRIGHT_RED, (586, 150), (586, 250), 8)
+        for display_text in self.display_messages:
+            display_text.render()
         for button in self.buttons:
             button.render()
 
     def __navigate_to_select_difficulty(self):
         self.game.set_scene(DifficultySelectScene(self.game))
 
+    def __navigate_to_multiplayer(self):
+        self.game.set_scene(MultiplayerScene(self.game))
+
     def __navigate_to_options(self):
         self.game.set_scene(OptionsScene(self.game))
+
+    def __add_single_player_message(self):
+        self.display_messages.append(
+            ui.DisplayText(
+                "This is a single player game",
+                random.random() * shm.DISPLAY_WIDTH,
+                random.random() * shm.DISPLAY_HEIGHT,
+                20,
+                shm.RED
+            )
+        )
 
 
 class OptionsScene(Scene):
     def __init__(self, game):
         self.game = game
+        self.display_messages = [
+            ui.DisplayText("THIS IS NOT DA WEI", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 2), 50, shm.GREEN)
+        ]
         self.buttons = [
             ui.Button("ABORT", shm.DISPLAY_WIDTH * 0.43, 450, 120, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_menu)
         ]
@@ -64,7 +87,8 @@ class OptionsScene(Scene):
     def render(self):
         shm.game_display.fill(shm.WHITE)
         shm.game_display.blit(shm.BURNING_KNUCKLES_IMAGE, (0, 0))
-        ui.draw_text("THIS IS NOT DA WEI", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 2), 50, shm.GREEN)
+        for display_text in self.display_messages:
+            display_text.render()
         for button in self.buttons:
             button.render()
 
@@ -75,6 +99,9 @@ class OptionsScene(Scene):
 class DifficultySelectScene(Scene):
     def __init__(self, game):
         self.game = game
+        self.display_messages = [
+            ui.DisplayText("Choose your difficulty", (shm.DISPLAY_WIDTH / 2), (150), 50, shm.RED)
+        ]
         self.buttons = [
             ui.Button("EAZY", shm.DISPLAY_WIDTH * 0.1 - 20, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, lambda: self.__navigate_to_hangman("eazy")),
             ui.Button("MEDIUM", shm.DISPLAY_WIDTH * 0.38, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, lambda: self.__navigate_to_hangman("medium")),
@@ -87,15 +114,63 @@ class DifficultySelectScene(Scene):
 
     def render(self):
         shm.game_display.fill(shm.WHITE)
-        ui.draw_text("Choose your difficulty", (shm.DISPLAY_WIDTH / 2), (150), 50, shm.RED)
         shm.game_display.blit(shm.EASY_DIFFICULTY_IMAGE, (shm.DISPLAY_WIDTH * 0.1, 250))
         shm.game_display.blit(shm.MEDIUM_DIFFICULTY_IMAGE, (shm.DISPLAY_WIDTH * 0.38, 250))
         shm.game_display.blit(shm.HARD_DIFFICULTY_IMAGE, (shm.DISPLAY_WIDTH * 0.7 - 10, 250))
+        for display_text in self.display_messages:
+            display_text.render()
         for button in self.buttons:
             button.render()
 
     def __navigate_to_hangman(self, difficulty):
         self.game.set_scene(HangmanScene(self.game, difficulty))
+
+
+class MultiplayerScene(Scene):
+    # TODO: finish implementation
+
+    def __init__(self, game):
+        self.game = game
+        self.socket = None
+        self.internet_image = pg.image.load(os.path.join("assets", "images", "internet.png"))
+        self.text_input = ui.TextInput(270, 380, 260, 40)
+        self.host_game_button = ui.Button("Host Game", shm.DISPLAY_WIDTH * 0.38, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__click_host_game)
+        self.buttons = [
+            ui.Button("Back", shm.DISPLAY_WIDTH * 0.1 - 20, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_menu),
+            self.host_game_button,
+            ui.Button("Connect to host", shm.DISPLAY_WIDTH * 0.7 - 20, 450, 200, 50, shm.GREEN, shm.BRIGHT_GREEN, lambda: None)
+        ]
+
+    def handle_event(self, event):
+        self.text_input.handle_event(event)
+        for button in self.buttons:
+            button.handle_event(event)
+
+    def render(self):
+        shm.game_display.fill(shm.WHITE)
+        shm.game_display.blit(self.internet_image, (shm.DISPLAY_WIDTH * 0.2, 80))
+        self.text_input.render()
+        for button in self.buttons:
+            button.render()
+
+    def __navigate_to_menu(self):
+        self.game.set_scene(MenuScene(self.game))
+
+    async def __click_host_game(self):
+        if self.socket is None:
+            tokens = self.text_input.content.split(":")
+            if len(tokens) != 2 or not tokens[1].isdigit():
+                return
+            host, port = tokens
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                self.host_game_button.display_text.update_message("Awaiting")
+                sock.bind((host, int(port)))
+                sock.listen()
+                conn, addr = sock.accept()
+                with conn:
+                    print(f"Connected by {addr}")
+        else:
+            self.host_game_button.display_text.update_message("Host Game")
 
 
 class HangmanScene(Scene):
@@ -104,7 +179,6 @@ class HangmanScene(Scene):
     MEDIUM_WORDS: List[str] = ["trampoline", "aftershock", "bankruptcy", "monarchist"]
     HARD_WORDS: List[str] = ["misconjugatedly", "dermatoglyphics"]
     LIVES: int = 7
-    MAX_INPUT_LENGTH = 21
 
     def __init__(self, game, difficulty):
         self.game = game
@@ -118,14 +192,22 @@ class HangmanScene(Scene):
         else:
             raise Exception("Invalid difficulty")
 
-        self.text_input = ""
+        self.text_input = ui.TextInput(50, 230, 260, 40)
         self.guessed_letters = set()
         self.hangman_text = " ".join(["_"] * len(self.word))
         self.failed_attempts = 0
         self.time = 0
+
         self.buttons = [
             ui.Button("Enter", 130, 300, 100, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__submit_guess)
         ]
+        self.guess_the_word_display_text = ui.DisplayText("Guess the Word!", 180, 100, 30, shm.RED)
+        self.hangman_display_text = ui.DisplayText(self.hangman_text, shm.DISPLAY_WIDTH * 0.5, shm.DISPLAY_HEIGHT - 100, 50, shm.RED)
+        self.time_label_display_text = ui.DisplayText("Time:", 538, 375, 20, shm.RED)
+        self.time_value_display_text = ui.DisplayText(str(int(self.time)), 625, 375, 20, shm.RED)
+        self.fails_label_display_text = ui.DisplayText("Fails:", 540, 400, 20, shm.RED)
+        self.num_fails_display_text = ui.DisplayText(str(self.failed_attempts) + "/" + str(HangmanScene.LIVES), 640, 400, 20, shm.RED)
+
         pg.mixer.music.load(os.path.join("assets", "sounds", "wii.mp3"))
         pg.mixer.music.play(-1)
 
@@ -133,28 +215,28 @@ class HangmanScene(Scene):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RETURN:
                 self.__submit_guess()
-            elif event.key == pg.K_BACKSPACE:
-                if len(self.text_input) > 0:
-                    self.text_input = self.text_input[:-1]
-            elif len(self.text_input) <= HangmanScene.MAX_INPUT_LENGTH:
-                user_input = event.dict.get("unicode")
-                self.text_input += user_input
+            else:
+                self.text_input.handle_event(event)
+
         for button in self.buttons:
             button.handle_event(event)
 
     def render(self):
-        shm.game_display.fill(shm.WHITE)
         self.time += 1/60
-        ui.draw_box(50, 310, 230, 270, 5)
+        self.time_value_display_text.update_message(str(int(self.time)))
+
+        shm.game_display.fill(shm.WHITE)
         ui.draw_box(shm.DISPLAY_WIDTH * 0.5, shm.DISPLAY_WIDTH - 25, 25, shm.DISPLAY_HEIGHT - 250, 5)
-        ui.draw_text("Guess the Word!", 180, 100, 30, shm.RED)
-        ui.draw_text(self.text_input, 180, 250, 20, shm.RED)
-        ui.draw_text(self.hangman_text, shm.DISPLAY_WIDTH * 0.5, shm.DISPLAY_HEIGHT - 100, 50, shm.RED)
-        ui.draw_text("Time:", 538, 375, 20, shm.RED)
-        ui.draw_text(str(int(self.time)), 625, 375, 20, shm.RED)
-        ui.draw_text("Fails:", 540, 400, 20, shm.RED)
-        ui.draw_text(str(self.failed_attempts) + "/" + str(HangmanScene.LIVES), 640, 400, 20, shm.RED)
         ui.draw_stand(480, 305)
+
+        self.guess_the_word_display_text.render()
+        self.hangman_display_text.render()
+        self.time_label_display_text.render()
+        self.time_value_display_text.render()
+        self.fails_label_display_text.render()
+        self.num_fails_display_text.render()
+        self.text_input.render()
+
         if self.failed_attempts >= 1:
             pg.draw.circle(shm.game_display, shm.BLACK, (600, 120), 25, 5)  # Head
             if self.failed_attempts >= 2:
@@ -167,23 +249,28 @@ class HangmanScene(Scene):
                             pg.draw.line(shm.game_display, shm.BLACK, (600, 200), (580, 235), 5)  # left leg
                             if self.failed_attempts >= 6:
                                 pg.draw.line(shm.game_display, shm.BLACK, (600, 200), (620, 235), 5)  # right leg
+
         for button in self.buttons:
             button.render()
 
     def __submit_guess(self):
-        if len(self.text_input) == 1 and self.text_input in self.word:
-            self.guessed_letters.add(self.text_input)
-            self.hangman_text = " ".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
+        text_content = self.text_input.content
 
-        if self.text_input == self.word or set(self.word) == self.guessed_letters:
+        if len(text_content) == 1 and text_content in self.word:
+            self.guessed_letters.add(text_content)
+            self.hangman_text = " ".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
+            self.hangman_display_text.update_message(self.hangman_text)
+
+        if text_content == self.word or set(self.word) == self.guessed_letters:
             self.game.set_scene(WinnerScene(self.game, self.time, self.failed_attempts))
-        elif self.text_input == "omae wa mou shindeiru":
+        elif text_content == "omae wa mou shindeiru":
             shm.MEME_VIDEO.preview()
             self.game.set_scene(WinnerScene(self.game, self.time, -9999999999))
-        elif self.text_input not in self.guessed_letters:
+        elif text_content not in self.guessed_letters:
             self.failed_attempts += 1
+            self.fails_label_display_text.update_message(str(self.failed_attempts))
 
-        self.text_input = ""
+        self.text_input.clear()
         if self.failed_attempts >= HangmanScene.LIVES:
             self.game.set_scene(LoserScene(self.game, self.time, self.failed_attempts))
 
@@ -191,8 +278,11 @@ class HangmanScene(Scene):
 class WinnerScene(Scene):
     def __init__(self, game, time, score):
         self.game = game
-        self.time = time
-        self.score = score
+        self.display_messages = [
+            ui.DisplayText("SUPREME WIN", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 150), 50, shm.GREEN),
+            ui.DisplayText("Time: " + str(int(time)) + " seconds", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 190), 25, shm.GREEN),
+            ui.DisplayText("Score: " + str(score) + " fails", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 220), 25, shm.GREEN)
+        ]
         self.buttons = [
             ui.Button("Quit", 150, 450, 120, 50, shm.GREEN, shm.BRIGHT_GREEN, quit_game),
             ui.Button("Menu", 550, 450, 120, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_menu)
@@ -205,9 +295,8 @@ class WinnerScene(Scene):
     def render(self):
         shm.game_display.fill(shm.BLUE)
         shm.game_display.blit(shm.WIN_IMAGE, (170, 15))
-        ui.draw_text("SUPREME WIN", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 150), 50, shm.GREEN)
-        ui.draw_text("Time: " + str(int(self.time)) + " seconds", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 190), 25, shm.GREEN)
-        ui.draw_text("Score: " + str(self.score) + " fails", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 220), 25, shm.GREEN)
+        for display_text in self.display_messages:
+            display_text.render()
         for button in self.buttons:
             button.render()
 
@@ -218,8 +307,11 @@ class WinnerScene(Scene):
 class LoserScene(Scene):
     def __init__(self, game, time, score):
         self.game = game
-        self.time = time
-        self.score = score
+        self.display_messages = [
+            ui.DisplayText("NO SUPREME FOR YOU", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3), 50, shm.RED),
+            ui.DisplayText("Time: " + str(int(time)) + " seconds", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 70), 25, shm.RED),
+            ui.DisplayText("Score: " + str(score) + " fails", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 100), 25, shm.RED)
+        ]
         self.buttons = [
             ui.Button("Quit", 150, 450, 120, 50, shm.GREEN, shm.BRIGHT_GREEN, quit_game),
             ui.Button("Menu", 550, 450, 120, 50, shm.GREEN, shm.BRIGHT_GREEN, self.__navigate_to_menu)
@@ -234,9 +326,8 @@ class LoserScene(Scene):
     def render(self):
         shm.game_display.fill(shm.WHITE)
         shm.game_display.blit(shm.LOSE_IMAGE, (0, 0))
-        ui.draw_text("NO SUPREME FOR YOU", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3), 50, shm.RED)
-        ui.draw_text("Time: " + str(int(self.time)) + " seconds", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 70), 25, shm.RED)
-        ui.draw_text("Score: " + str(self.score) + " fails", int(shm.DISPLAY_WIDTH / 2), int(shm.DISPLAY_HEIGHT / 3 + 100), 25, shm.RED)
+        for display_text in self.display_messages:
+            display_text.render()
         for button in self.buttons:
             button.render()
 
--- supreme_hangman/user_interface.py
+++ supreme_hangman/user_interface.py
@@ -4,7 +4,7 @@ import supreme_hangman as shm
 
 class Button:
     def __init__(self, text, left, top, width, height, background_color, hover_color, on_click=None):
-        self.text = text
+        self.display_text = DisplayText(text, left + width / 2, top + height / 2, 20, shm.RED)
         self.left = left
         self.top = top
         self.width = width
@@ -30,14 +30,66 @@ class Button:
             pg.draw.rect(shm.game_display,
                          self.background_color,
                          (self.left, self.top, self.width, self.height))
-        button_font = pg.font.Font("freesansbold.ttf", 20)
-        text_surf, text_rect = create_text_surface(self.text, button_font, shm.RED)
-        text_rect.center = ((self.left + (self.width / 2)), (self.top + (self.height / 2)))
-        shm.game_display.blit(text_surf, text_rect)
+        self.display_text.render()
 
     def __update_is_hovered(self):
         mouse_x, mouse_y = pg.mouse.get_pos()
         self.is_hovered = self.left < mouse_x < self.left + self.width and self.top < mouse_y < self.top + self.height
+
+
+class TextInput:
+    FONT = pg.font.Font("freesansbold.ttf", 20)
+    MAX_INPUT_LENGTH = 21
+
+    def __init__(self, left, top, width, height):
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.content = ""
+        self.__update_display_text()
+
+    def handle_event(self, event: pg.event.Event):
+        if event.type != pg.KEYDOWN or event.key == pg.K_RETURN:
+            return
+        if event.key == pg.K_BACKSPACE:
+            self.content = self.content[:-1]
+        elif len(self.content) <= TextInput.MAX_INPUT_LENGTH:
+            user_input = event.dict.get("unicode")
+            self.content += user_input
+
+        self.__update_display_text()
+
+    def render(self):
+        draw_box(self.left, self.left + self.width, self.top, self.top + self.height, 5)
+        shm.game_display.blit(self.text_surface, self.text_rect)
+
+    def clear(self):
+        self.content = ""
+        self.__update_display_text()
+
+    def __update_display_text(self):
+        self.text_surface = TextInput.FONT.render(self.content, True, shm.RED)
+        self.text_rect = self.text_surface.get_rect()
+        self.text_rect.midleft = (self.left + 5, self.top + self.height / 2)
+
+
+class DisplayText:
+    def __init__(self, message, x, y, size, color):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        self.update_message(message)
+
+    def render(self):
+        shm.game_display.blit(self.text_surface, self.text_rect)
+
+    def update_message(self, message):
+        font = pg.font.Font("freesansbold.ttf", self.size)
+        self.text_surface = font.render(message, True, self.color)
+        self.text_rect = self.text_surface.get_rect()
+        self.text_rect.center = (self.x, self.y)
 
 
 def draw_box(x1, x2, y1, y2, w):
@@ -52,15 +104,3 @@ def draw_stand(x, y):
     pg.draw.line(shm.game_display, shm.BLACK, (x - 40, y), (x + 80, y), 5)
     pg.draw.line(shm.game_display, shm.BLACK, (x, y - 240), (x + 120, y - 240), 5)
     pg.draw.line(shm.game_display, shm.BLACK, (x + 120, y - 240), (x + 120, y - 210), 5)
-
-
-def create_text_surface(text, font, color):
-    text_surface = font.render(text, True, color)
-    return text_surface, text_surface.get_rect()
-
-
-def draw_text(msg, x, y, size, color):
-    small_text = pg.font.Font("freesansbold.ttf", size)
-    text_surf, text_rect = create_text_surface(msg, small_text, color)
-    text_rect.center = (x, y)
-    shm.game_display.blit(text_surf, text_rect)
